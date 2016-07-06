920e6548ef553f183fafa130a16415f3cebff4b6
Jeremy Fan <jeremyfan_14@live.com>
2016-07-06 19:57:19 +0800

    handle session

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var port = 8005;
 
 var nodemailer = require('nodemailer');
 var mongoose = require('mongoose');
+var session = require('express-session');
 
 var Account = require('./models/account')(mongoose, nodemailer);
 
@@ -11,11 +12,23 @@ mongoose.connect('mongodb://localhost/socialnet');
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
+app.use(session({
+	secret: 'crazy world',
+	resave: true,
+	saveUninitialized: true
+}));
 
 app.get('/', function(req, res) {
 	res.render('index.pug', {
 		layout: false
 	});
+});
+
+app.get('/account/authenticated', function(req, res) {
+	if (req.session.loggedIn)
+		res.sendStatus(200);
+	else
+		res.sendStatus(401);
 });
 
 app.listen(port);
