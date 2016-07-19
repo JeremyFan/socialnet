f1f05372072d620c134268fd29e889d161e4f300
Jeremy Fan <jeremyfan_14@live.com>
2016-07-19 22:19:28 +0800

    profile view&template

---
+++ public/js/views/profile.js
@@ -0,0 +1,28 @@
+define(['socialnet-view', 'text!templates/profile.html', 'models/status', 'views/status'],
+	function(SocialNetView, profileTpl, Status, StatusView) {
+		var profileView = SocialNetView.extend({
+			el: $('#content'),
+
+			initialize: functon() {
+				this.model.on('change', this.render, this);
+			},
+
+			render: function() {
+				this.$el.html(_.template(profileTpl, this.model.toJSON()));
+
+				var statusCollection = this.model.get('status');
+				if (null != statusCollection) {
+					_.each(statusCollection, function(statusJson) {
+						var statusModel = new Status(statusJson);
+						var statusView = new StatusView({
+							model: statusModel
+						});
+						var statusHtml = statusView.render().el;
+						$(statusHtml).prependTo('.status_list').hide().fadeIn('slow');
+					});
+				}
+			}
+		});
+
+		return profileView;
+	});
