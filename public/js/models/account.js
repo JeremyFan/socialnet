5117c1e5cb8949a6ecfd940f9cca671a1e87ca3d
Jeremy Fan <jeremyfan_14@live.com>
2016-07-23 23:16:46 +0800

    add comment

--- public/js/models/account.js
+++ public/js/models/account.js
@@ -1,5 +1,6 @@
 define(['models/status-collection'], function(StatusCollection) {
 	var Account = Backbone.Model.extend({
+		// Specify a urlRoot if you're using a model outside of a collection, to enable the default url function to generate URLs based on the model id. "[urlRoot]/id"
 		urlRoot: '/accounts',
 
 		initialize: function() {
